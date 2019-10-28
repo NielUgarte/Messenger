891ca15bf70a86a41441f9138562ada2f9552ed9
Niel Ugarte Macedo <nugartemacedo@gmail.com>
2019-10-28 18:19:39 -0500

    navigation basic between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
